12ca16f78b41a0c1a40589cda7898bdc5224c340
rlaehgml0830 <kimdohui05@gmail.com>
2026-03-12 14:00:46 +0900

    Add User Login Service

---
+++ src/main/java/com/AIPM/AIPM_Back/AipmApplication.java
@@ -0,0 +1,13 @@
+package com.AIPM.AIPM_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AipmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AipmApplication.class, args);
+	}
+
+}
